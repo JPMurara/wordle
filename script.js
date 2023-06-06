7a93b1effda364fe73ebbe8f78136d1704c336c6
Joao Murara <joaopaulo.murara@gmail.com>
2023-06-06 23:07:30 +1000

    refactoring the checkWord function

--- script.js
+++ script.js
@@ -21,8 +21,10 @@ btnReset.addEventListener("click", resetGame);
 
 function isValidGuess() {
   if (input.value.includes(" ")) {
+    //donest accept spaces in the input value
     console.log("Invalid entry. Can't include spaces");
   } else if (/\d/.test(input.value)) {
+    //doesnt accept numbers in the input value
     console.log("Invalid entry. Can't include numbers");
   } else {
     submitGuess();
@@ -38,62 +40,61 @@ function submitGuess() {
       //iterates over the indexes of the guess array. The index is used to iterate over the children/divs and the guess array elements
       //console.log(letterIndex); //consoles the children(div) index from 0 to 4
       row[rowIndex].children[letterIndex].textContent = guess[letterIndex]; //each div text content is assigned to each of the guess array element(letter)
-      input.value = ""; //remove from here, put outside of the loop
     }
     isValidAttempt(); //checks if the player still have attempts to go
     checkWord(guess); //calls the function to check each of the letters againts the answer array
   } else {
     console.log("The word has to have 5 letters"); //doesnt accepts words less than 5 letters
   }
-  isWin(guess); //pass the input.value here
+  input.value = "";
+  isWin(guess); //pass the input.value to the isWin function
+}
+
+function checkWord(array) {
+  //the guess array is passed into this function as an argument
+  const answerArr = Array.from(answer); //upper case the answer to make the comparsion with the guess possible, creates an array with each of the answer variable letters
+  for (const letterIndex in answerArr) {
+    if (answerArr[letterIndex] === array[letterIndex]) {
+      //compares if the guess letters are in the correct position and applies the green class
+      row[rowIndex].children[letterIndex].classList.add("green");
+      answerArr[letterIndex] = " "; //replaces the matching letter with empty space so in the next loop, the yellow isn't applied to the same div letter as well
+    }
+  }
+  for (const letterIndex in answerArr) {
+    if (answerArr.includes(array[letterIndex])) {
+      //if the guess includes any of the correct letters but not in the correct position, applies the class yellow.
+      row[rowIndex].children[letterIndex].classList.add("yellow");
+    }
+  }
+  rowIndex++; //increments the row number so we can jump into the new row after the loop for the letter is over in a specific row. Works the same as using the attemptCount but I want to create another variable just to make the code more clear
 }
 
 function isValidAttempt() {
-  attempsCount++; //increments the attemps
-  if (attempsCount > 5) {
+  //checks if the player still have a valid attempt to play and calls the gameOver function
+  attempsCount++;
+  if (attempsCount === 6) {
     gameOver();
   }
 }
 
 function gameOver() {
+  //display the game over message and disable the input and submit button
   console.log(`Game Over! Better luck next time, the word is: ${answer}`);
   input.disabled = true;
   btnSubmit.disabled = true;
 }
 
 function isWin(guess) {
+  //checks if the guess matches with the answer, display a message and disable the input and button
   if (answer === guess.join("")) {
     console.log("Congratulations! You win the game!");
     input.disabled = true;
     btnSubmit.disabled = true;
-    input.value = "";
   }
-}
-
-function checkWord(array) {
-  //the guess array is passed into this function as an argument
-  const answerArr = Array.from(answer); //upper case the answer to make the comparsion with the guess possible, creates an array with each of the answer variable letters
-  for (let i = 0; i < answerArr.length; i++) {
-    //can use in here as well
-    //this loop increments the i variable so we can compare elements on both array at the same index
-    if (answerArr[i] === array[i]) {
-      //compares if the guess letters are in the correct position and applies the green class
-      row[rowIndex].children[i].classList.add("green");
-      answerArr[i] = " "; //explain this
-    }
-  }
-  for (let i = 0; i < answerArr.length; i++) {
-    //can use in here as well
-    if (answerArr.includes(array[i])) {
-      //if the guess includes any of the correct letters but not in the correct position, applies the class yellow. The previous condition doesnt affect this one. Once the previous condition is met, the code goes back into the begining of the loop to check the next index, so this else if is skipped
-      row[rowIndex].children[i].classList.add("yellow");
-    }
-  }
-  rowIndex++; //increments the row number so we can jump into the new row after the loop for the letter is over in a specific row. Works the same as using the attemptCount but I want to create another variable just to make the code more clear
-  return answerArr;
 }
 
 function resetGame() {
+  //reset the game, set variables back to its initial values, anable button and input, clear divs text content
   attempsCount = 0;
   rowIndex = 0;
   for (let div of divs) {
@@ -102,6 +103,4 @@ function resetGame() {
   }
   input.disabled = false;
   btnSubmit.disabled = false;
-  input.value = "";
-  //resert the rowindex
 }
