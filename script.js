23a78602fbd45f7daa7f9b6cc9e1bcd267316d47
Joao Murara <joaopaulo.murara@gmail.com>
2023-06-10 10:25:51 +1000

    removing comments and re-organizing js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //https://gist.github.com/katharosada/ebb779cf90f26774e6ecf3f8eef4c317
 
+//getting HTML elements
 const btnSubmit = document.getElementById("btn-submit");
 const btnReset = document.getElementById("btn-reset");
 const input = document.getElementById("input");
@@ -10,27 +11,33 @@ const btnHideMessage = document.querySelectorAll(".btn-hide-message");
 const messageGameOver = document.querySelector("#message-game-over");
 const paragraphGameOverMessage = document.querySelector("#p-game-over");
 const messageWin = document.querySelector("#message-win");
-// const keyboard = document.querySelectorAll(".key");
-// let answer = validWords[Math.floor(Math.random() * validWords.length) + 1];
-// console.log(answer);
+
+//variables that are used in functions
 let rowIndex = 0;
 let attempsCount = 0;
 
+//generates a random answer: chooses a word from the validWords array by creating a random index
 function randomAnswer(validWords) {
   let answer = validWords[Math.floor(Math.random() * validWords.length) + 1];
-  console.log(answer);
+  console.log(answer); //consoles the answer so we can cheat!
   return answer; //sends the answer value back to the caller
 }
-let answer = randomAnswer(validWords); //capture the return value (answer) when the fucntion is called and assings it to a variable, so we can use it thourghout the code
+let answer = randomAnswer(validWords); //capture the return value (answer) when the fucntion is called and assings it to a variable, so it can be used thourghout the code
 
+//event listeners
 btnSubmit.addEventListener("click", isValidGuess);
 btnReset.addEventListener("click", resetGame);
 
-// for (let el of keyboard) {
-//   el.addEventListener("click", getLetter);
-// }
+for (const btn of btnHideMessage) {
+  //adds the event listener to all the buttons
+  btn.addEventListener("click", (e) => {
+    //hides the dialog html tag by removing the attribute from the parent element of the button clicked using the close() function
+    e.target.parentElement.close();
+  });
+}
 
 function isValidGuess() {
+  //checks if the guess is valid against some rules
   if (
     input.value.includes(" ") || //checks spaces
     /\d/.test(input.value) || //checks if input includes numbers
@@ -39,41 +46,38 @@ function isValidGuess() {
     messageInvalidEntry.show(); //set the open attribute to the dialog html element. Show message if criteria is not met
     input.value = "";
   } else {
-    submitGuess(); //otherwise submits the guess
+    submitGuess(); //otherwise calls the submits the guess function
   }
-}
-
-for (const btn of btnHideMessage) {
-  //adds the event listener to all the buttons
-  btn.addEventListener("click", (e) => {
-    //hides the dialog html tag by removing the attribute from the parent element of the button clicked
-    e.target.parentElement.close();
-  });
 }
 
 function submitGuess() {
   const guess = Array.from(input.value.toUpperCase()); //upper case the guess to make the comparsion with the answer possible, creates an array with each of the guess variable letters
-  //check the min length. Doesn't let the player input less than 5 letters word
-  //console.log(guess); //consoles an array with all the letter from the input
   for (const letterIndex in guess) {
     //iterates over the indexes of the guess array. The index is used to iterate over the children/divs and the guess array elements
-    //console.log(letterIndex); //consoles the children(div) index from 0 to 4
     row[rowIndex].children[letterIndex].textContent = guess[letterIndex]; //each div text content is assigned to each of the guess array element(letter)
   }
   isValidAttempt(); //checks if the player still have attempts to go
-  checkWord(guess); //calls the function to check each of the letters againts the answer array
+  styleLetters(guess); //calls the function to check each of the letters againts the answer array
   input.value = "";
   isWin(guess); //pass the input.value to the isWin function
 }
 
-function checkWord(array) {
-  //the guess array is passed into this function as an argument
+function isValidAttempt() {
+  //checks if the player still have a valid attempt to play and calls the gameOver function
+  attempsCount++;
+  if (attempsCount === 6) {
+    gameOver();
+  }
+}
+
+function styleLetters(array) {
+  //the guess array is passed into this function as an argument. The function checks each of the letter and apply styles if condition is met
   const answerArr = Array.from(answer); //upper case the answer to make the comparsion with the guess possible, creates an array with each of the answer variable letters
   for (const letterIndex in answerArr) {
     if (answerArr[letterIndex] === array[letterIndex]) {
-      //compares if the guess letters are in the correct position and applies the green class
+      //compares if the guess letter is in the correct position and applies the style class green
       row[rowIndex].children[letterIndex].classList.add("green");
-      answerArr[letterIndex] = " "; //replaces the matching letter with empty space so in the next loop, the yellow isn't applied to the same div letter as well
+      answerArr[letterIndex] = " "; //replaces the matching letter with empty space so in the next loop, the yellow class isn't applied to the same div
     }
   }
   for (const letterIndex in answerArr) {
@@ -85,29 +89,22 @@ function checkWord(array) {
   rowIndex++; //increments the row number so we can jump into the new row after the loop for the letter is over in a specific row. Works the same as using the attemptCount but I want to create another variable just to make the code more clear
 }
 
-function isValidAttempt() {
-  //checks if the player still have a valid attempt to play and calls the gameOver function
-  attempsCount++;
-  if (attempsCount === 6) {
-    gameOver();
-  }
-}
-
-function gameOver() {
-  //display the game over message and disable the input and submit button
-  paragraphGameOverMessage.innerHTML = `Better luck next time, the word is: ${answer}`;
-  messageGameOver.show();
-  input.disabled = true;
-  btnSubmit.disabled = true;
-}
-
 function isWin(guess) {
-  //checks if the guess matches with the answer, display a message and disable the input and button
+  //checks if the guess matches with the answer, display the wining message and disable the input and button
   if (answer === guess.join("")) {
+    //concatenates the guess array and turns it into a string so it can be compared to the answer string. Since there is no separator in the join method, the guess array is cocatenated all together
     messageWin.show();
     input.disabled = true;
     btnSubmit.disabled = true;
   }
+}
+
+function gameOver() {
+  //display the game over message and disable the input and submit button if the player exceeded the number of attempts
+  paragraphGameOverMessage.innerHTML = `Better luck next time, the word is: ${answer}`;
+  messageGameOver.show();
+  input.disabled = true;
+  btnSubmit.disabled = true;
 }
 
 function resetGame() {
@@ -120,9 +117,5 @@ function resetGame() {
   }
   input.disabled = false;
   btnSubmit.disabled = false;
-  location.reload(); //js method to reload the current URL(location)
+  location.reload(); //method to reload the current URL(location), so js can run the random answer function. Otherwise the answer is the same as the previous game
 }
-
-// function getLetter(e) {
-//   console.log(e.target.textContent.toUpperCase());
-// }
